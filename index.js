f3b38d915302f4af0aeacb22598f420ba5ada5f2
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-16 20:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(currentLine, newName) {
-  return `Welcome, ${newName}. You are number ${currentLine} += 1`;
+  return `Welcome, ${newName}. You are number ${currentLine} += 1 in line`;
 }
