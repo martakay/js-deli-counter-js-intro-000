c7674eec07791de829381d243472b7ff73009b46
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-16 22:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine) {
 function currentLine(currentLine) {
   if ( currentLine.length > 0 ) {
     for (var i = 0; i < currentLine.length; i++) {
-      return `The line is currently: ${i}. ${currentLine[i]}`
+      `The line is currently: ${i}. ${currentLine[i]}`
     }
   }
   else {
