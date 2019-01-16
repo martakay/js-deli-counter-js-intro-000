5c4340e884f3e56a2d7f7168dcb916ce94713276
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-16 21:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(currentLine, newName) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}/`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
     katzDeliLine.shift();
   }
   else {
