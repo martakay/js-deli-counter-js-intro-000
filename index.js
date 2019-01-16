154e4ea70d878dc85cdaf181b505922dddd6e7d7
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-16 21:31:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function takeANumber(currentLine, newName) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`;
+    katzDeliLine.shift();
   }
   else {
     return "There is nobody waiting to be served!";
